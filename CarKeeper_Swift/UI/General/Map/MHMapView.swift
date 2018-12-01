68dcff5c9ac87d327e8f2a0260ee2ef0de5c4428
huang <mh@my-mac-mini.local>
2018-12-01 11:59:58 +0800

    搭好框架

---
+++ CarKeeper_Swift/UI/General/Map/MHMapView.swift
@@ -0,0 +1,21 @@
+//
+//  MHMapView.swift
+//  CarKeeper_Swift
+//
+//  Created by huang on 2018/12/1.
+//  Copyright © 2018年 henry. All rights reserved.
+//
+
+import UIKit
+
+class MHMapView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ CarKeeper_Swift/global.swift
@@ -0,0 +1,9 @@
+//
+//  global.swift
+//  CarKeeper_Swift
+//
+//  Created by huang on 2018/12/1.
+//  Copyright © 2018年 henry. All rights reserved.
+//
+
+import Foundation
